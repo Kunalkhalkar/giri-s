5f4bcc268a82eefbd08b8a6bf4a86c820d132c65
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-17 08:29:51 +0530

    assignment 3

---
+++ AsciiExtract.java
@@ -0,0 +1,15 @@
+/*Q16. Write a Java program to print the ASCII value of a given character.
+*/
+import java.util.*;
+public class AsciiExtract{
+
+public static void main(String x[]){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter a charactor which you want ascii value");
+char ch=sc.next().charAt(0);
+int AS = ch;
+System.out.println("Ascii value is = "+AS);
+
+}
+
+}
