1d2e4a3a8feed80f83882bb54c58489c4ebd3df4
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-18 22:01:05 +0530

    Assignment 3

--- quadrant.java
+++ quadrant.java
@@ -1,1 +1,13 @@
 /*Q56. Given (x, y), print quadrant I, II, III, IV or "Axis".*/
+
+import java.util.*;
+public class quadrant{
+	public static void main(String x[]){
+	System.out.println("Enter vslue of x & y");
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	int y =sc.nextInt();
+
+}
+}
+
